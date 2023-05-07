d99900f166b14f35b53613d296b3ee4dfeef7225
Abhinav Chaudhary <111004089+ChaudharyAbhinav@users.noreply.github.com>
2023-05-08 04:54:29 +0530

    Add files via upload

--- Group B assignments/Assignment3B/mapper.py
+++ Group B assignments/Assignment3B/mapper.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python
+# import sys
+# for line in sys.stdin.buffer:
+#     line = line.strip()
+#     words = line.split()
+#     for word in words:
+#         print("%s\t%s" % (word, 1))
+
 import sys
 import io
 import codecs
@@ -9,3 +17,10 @@ for line in input_stream:
     words = line.split()
     for word in words:
        print("%s\t%s" %(word,1))
+  
+#   cat input.rtf |python mapper.py
+# cat input.rtf |python mapper.py |sort| python reducer.py
+# hdfs dfs -mkdir /hadpy
+# hdfs dfs -copyFromLocal /home/abhi/input.txt /hadpy
+#   hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.4.jar -input /hadpy/wordc.txt -output /hadout/output -file mapper.py -file reducer.py -mapper 'python3 mapper.py' -reducer 'python3 reducer.py'
+  
