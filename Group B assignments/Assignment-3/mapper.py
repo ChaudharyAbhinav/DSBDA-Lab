8336dc03263275666f6ac56d951cdf9c3e52daab
Abhinav Chaudhary <111004089+ChaudharyAbhinav@users.noreply.github.com>
2023-05-24 23:46:40 +0530

    Update mapper.py

--- Group B assignments/Assignment-3/mapper.py
+++ Group B assignments/Assignment-3/mapper.py
@@ -1,3 +1,5 @@
+//python 2 code:    use this code if u have python 2
+
 import sys
 import io
 import codecs
@@ -9,3 +11,16 @@ for line in input_stream:
     words = line.split()
     for word in words:
        print("%s\t%s" %(word,1))
+    
+    
+    
+//python 3 code:    use this code if u have python 3
+import sys
+import io
+input_stream = io.TextIOWrapper(sys.stdin.buffer)
+
+for line in input_stream:
+	line = line.lower()
+	words = line.split()
+	for word in words:
+		print("%s\t%s" %(word,1))
